8d2b61243630734578f03c6a92cc67cde23d5188
anand bagul <anandrbagul@gmail.com>
2022-11-12 22:21:03 +0530

    First Commit

---
+++ New_Test.py
@@ -0,0 +1,32 @@
+# ages = [5, 12, 17, 18, 24, 32]
+#
+# def myFunc(x):
+#   if x < 18:
+#     return False
+#   else:
+#     return True
+#
+# adults = filter(myFunc, ages)
+#
+# for x in adults:
+#   print(x)
+
+
+Numbers = [10, 25, 45, 78, 5, 65, 8, 102]
+
+
+# def demo1():
+#     if Numbers > 20:
+#         return True
+#     else:
+#         return False
+#
+# segregation = filter(demo1, Numbers)
+#
+# for i in segregation:
+#   print(segregation)
+
+
+
+
+
